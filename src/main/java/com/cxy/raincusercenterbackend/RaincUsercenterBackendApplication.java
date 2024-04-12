05b12c376090436ab05ec1713081c3a19eb1ab9f
starlightcxy <3050094865@qq.com>
2024-04-12 13:55:42 +0800

    'first'

---
+++ src/main/java/com/cxy/raincusercenterbackend/RaincUsercenterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cxy.raincusercenterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaincUsercenterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaincUsercenterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cxy/raincusercenterbackend/RaincUsercenterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxy.raincusercenterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaincUsercenterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
